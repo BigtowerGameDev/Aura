068f13484ce2c95428ef02dc0813770bf76b567f
Toni Torregrosa <112747749+BigtowerGameDev@users.noreply.github.com>
2024-10-09 14:04:09 +0200

    broadcasting

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraCommonUserWidget.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
+
+UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetControllerParams& WCParams)
+{
+	if(OverlayWidgetController == nullptr)
+	{
+		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
+		OverlayWidgetController->SetWidgetControllerParams(WCParams);
+
+		return OverlayWidgetController;
+	}
+	return OverlayWidgetController;
+}
+
+void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS)
+{
+	checkf(OverlayWidgetClass, TEXT("Overlay Widget Class uninitialized, please fill out BP_AuraHUD"));
+	checkf(OverlayWidgetControllerClass, TEXT("Overlay Widget Controller Class uninitialized, please fill out BP_AuraHUD"));
+	
+	UAuraCommonUserWidget* Widget = CreateWidget<UAuraCommonUserWidget>(GetWorld(), OverlayWidgetClass);
+
+	const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+	UOverlayWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
+
+	Widget->SetWidgetController(WidgetController);
+	WidgetController->BroadcastInitiaValues();
+	Widget->AddToViewport();
+}
